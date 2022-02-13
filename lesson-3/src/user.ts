8c517479c54644fac708937ec224cb007fd6b4a1
Dzmitry85 <dpinchuk85@gmail.com>
2022-02-13 19:54:36 +0300

    Пркатическое задание-3

---
+++ lesson-3/src/user.ts
@@ -0,0 +1,40 @@
+import { renderBlock } from './lib.js'
+
+
+
+ interface user {
+  name: string,
+  avatarUrl: string
+}
+
+export const getUserData: user = {
+  name: 'usrName',
+  avatarUrl: 'avatar.png',
+};
+
+
+ type favouritesAmount = number;
+export const getFavoritesAmount: favouritesAmount = 1;
+
+
+
+
+export const renderUserBlock = (user: user, faves?: favouritesAmount) => {
+  const favoritesCaption = faves ? faves : 'Ничего нет';
+  const hasFavoriteItems = faves > 0 ? true : false;
+
+  renderBlock(
+    'user-block',
+    `
+      <div class="header-container">
+        <img class="avatar" src="/img/${user.avatarUrl}" alt="${user.name}"/>
+        <div class="info">
+          <p class="name">${user.name}</p>
+          <p class="fav">
+            <i class="heart-icon${hasFavoriteItems ? ' active' : ' inactive'}"></i>${favoritesCaption}
+          </p>
+        </div>
+      </div>
+    `
+  );
+};
